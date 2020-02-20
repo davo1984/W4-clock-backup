86d50a825a475439ff6cd0920a6e4863924d4c5b
David Oliver <davooliver1964@gmail.com>
2020-02-20 15:52:41 -0500

    fix AM-Noon bug

--- js/main.js
+++ js/main.js
@@ -9,44 +9,39 @@ let minute = dte.getMinutes();
 let second = dte.getSeconds();
 let strM, strH, strS, dateString = '';
 
+//testing
+//hour = 13;
+
 //get the hour; convert to 12 hour format; set am or pm; pad if needed
-if (hour > 12) {
-    hour = hour - 12;
-    if (hour <10) {
-        strH = '0' + hour;
-    } else {
-        strH = hour;
-    }
+if (hour === 0) {
+    amPM = 'AM';
+    hour = 12;
+} else if (hour < 12) {
+    amPM = 'AM';
+} else if (hour === 12) {
     amPM = 'PM';
-} else if (hour = 0) {
-    strH = 12;
-    amPM = 'AM';
-} else {
-    amPM = 'AM';
-    if (hour < 10) {
-        strH = '0' + hour;
-    } else {
-        strH = hour;
-    }
-}
+} else if (hour > 12) {
+    amPM = 'PM';
+    hour = hour - 12;
+} 
 
-//pad minute if needed
-if (minute < 10) {
-    strM = '0' + minute;
-} else {
-    strM = minute;
-}
-
-//pad second if needed
-if (second < 10) {
-    strS = '0' + second;
-} else {
-    strS = second;
-}
+//pad hour, minute, second if needed
+strH = addPad(hour, ' ');
+strM = addPad(minute, '0');
+strS = addPad(second, '0');
 
 dateString = strH + ':' + strM + ':' + strS + ' ' +amPM;
 
 document.getElementById("clockHere").innerHTML = dateString;
+}
+
+function addPad(n, p) {
+    if (n < 10) {
+        strN = p + n;
+    } else {
+        strN = n;
+    }
+    return strN;
 };
 
 window.onload=function(){
